b41f5fe75acce7a7f14ec648591d567323cb4844
Grzegorz Wiktorzak <wiktorzak.greg@gmail.com>
2021-03-30 09:57:12 +0200

    employee repository and sessions controller

--- FoodDelivery_app/app.rb
+++ FoodDelivery_app/app.rb
@@ -3,17 +3,17 @@ require_relative 'app/repositories/meal_repository'
 require_relative 'app/controllers/meals_controller'
 require_relative 'app/repositories/customer_repository'
 require_relative 'app/controllers/customers_controller'
-require_relative "app/repositories/employee_repository"
-require_relative "app/controllers/sessions_controller"
+require_relative 'app/repositories/employee_repository'
+require_relative 'app/controllers/sessions_controller'
 require_relative 'router'
 
 # files
 MEALS_CSV_FILE = File.join(__dir__, 'data/meals.csv')
 CUSTOMERS_CSV_FILE = File.join(__dir__, 'data/customers.csv')
-EMPLOYEES_CSV_FILE = File.join(__dir__, "data/employees.csv")
+EMPLOYEES_CSV_FILE = File.join(__dir__, 'data/employees.csv')
 
 # meals
-meal_repository = MealRepository.new(MEALS_CSV_FILE )
+meal_repository = MealRepository.new(MEALS_CSV_FILE)
 meals_controller = MealsController.new(meal_repository)
 
 # customers
@@ -28,4 +28,3 @@ sessions_controller = SessionsController.new(employee_repository)
 router = Router.new(meals_controller, customers_controller, sessions_controller)
 # start the app
 router.run
-
