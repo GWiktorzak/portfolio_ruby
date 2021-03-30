c26153c2ae895dafb31c88da42d5abb717021901
Grzegorz Wiktorzak <wiktorzak.greg@gmail.com>
2021-03-30 10:57:20 +0200

    changed view for meals_view fixed spec errors

---
+++ FoodDelivery_app/app/views/meals_view.rb
@@ -0,0 +1,13 @@
+class MealsView
+  def ask_user_for(stuff)
+    puts "#{stuff.capitalize}?"
+    print '> '
+    gets.chomp
+  end
+
+  def display(meals)
+    meals.each_with_index do |meal, index|
+      puts "#{index + 1} - #{meal.name} : #{meal.price}€"
+    end
+  end
+end
